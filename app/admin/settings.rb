684d09b7581fd4b73675c28b57f0eb272b6168b9
anonymous anonymous <emi.rosanwo@kellogg.northwestern.edu>
2022-03-10 17:55:50 +0000

    Whitelist columns in Setting for ActiveAdmin

--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Setting do
 
+ permit_params :owner_info, :billing, :billing_card, :domains, :profile_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
