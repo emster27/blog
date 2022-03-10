46f22334b777838483ea4ec6b5afff0a51faf9fb
anonymous anonymous <emi.rosanwo@kellogg.northwestern.edu>
2022-03-10 17:55:27 +0000

    Generate social_links resource with ideas:scaffold

---
+++ app/models/social_link.rb
@@ -0,0 +1,14 @@
+class SocialLink < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    username
+  end
+
+end
---
+++ app/views/social_links/_social_link.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! social_link, :id, :username, :medium, :password, :profile_id, :created_at, :updated_at
+json.url social_link_url(social_link, format: :json)
