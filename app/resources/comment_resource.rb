5ff22300103bd953f6118902dbffebf335cb3239
anonymous anonymous <emi.rosanwo@kellogg.northwestern.edu>
2022-03-10 17:56:52 +0000

    Generate graphiti resources for comments

---
+++ app/resources/comment_resource.rb
@@ -0,0 +1,14 @@
+class CommentResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :page_id, :integer
+  attribute :reader_id, :integer
+  attribute :reader_name, :string
+  attribute :reader_email, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
