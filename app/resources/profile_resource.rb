867b70f9be940cc138927d294621b1b4b1835a36
anonymous anonymous <emi.rosanwo@kellogg.northwestern.edu>
2022-03-10 17:56:57 +0000

    Generate graphiti resources for profiles

---
+++ app/resources/profile_resource.rb
@@ -0,0 +1,14 @@
+class ProfileResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :email, :string
+  attribute :password, :string
+  attribute :account_type, :string
+  attribute :dob, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
